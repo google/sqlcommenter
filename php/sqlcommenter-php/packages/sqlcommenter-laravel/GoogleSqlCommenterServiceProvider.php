68efb74369210ced2370622a8cd698370e3d0872
Thiyagu55 <thiyagu.nataraj@searce.com>
2022-03-18 15:54:04 +0530

    Initializing package

---
+++ php/sqlcommenter-php/packages/sqlcommenter-laravel/GoogleSqlCommenterServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace thiyagu55\GoogleSqlcommenterLaravel;
+
+class GoogleSqlCommenterServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+}
