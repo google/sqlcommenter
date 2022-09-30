7ba7ca1f51594db42819172b26b6064b615099ae
Thiyagu55 <64461612+Thiyagu55@users.noreply.github.com>
2022-09-30 19:29:59 +0530

    go-sql refactoring (#165)

--- go/go-sql/go-sql_test.go
+++ go/go-sql/go-sql_test.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package gosql
 
 import (
@@ -7,20 +21,18 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/sqlcommenter/go/core"
+	httpnet "github.com/google/sqlcommenter/go/net/http"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-var engine, connectionParams = "mysql", "root:root@/gotest"
-
 func TestDisabled(t *testing.T) {
 	mockDB, _, err := sqlmock.New()
-
-	db := DB{DB: mockDB, options: CommenterOptions{}}
 	if err != nil {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
-
+	db := DB{DB: mockDB, options: core.CommenterOptions{}}
 	query := "SELECT 2"
 	if got, want := db.withComment(context.Background(), query), query; got != want {
 		t.Errorf("db.withComment(context.Background(), %q) = %q, want = %q", query, got, want)
@@ -29,59 +41,61 @@ func TestDisabled(t *testing.T) {
 
 func TestHTTP_Net(t *testing.T) {
 	mockDB, _, err := sqlmock.New()
-
-	db := DB{DB: mockDB, options: CommenterOptions{EnableDBDriver: true, EnableRoute: true, EnableFramework: true}}
 	if err != nil {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
 
-	r, _ := http.NewRequest("GET", "hello/1", nil)
-	ctx := AddHttpRouterTags(r, context.Background())
+	db := DB{DB: mockDB, options: core.CommenterOptions{EnableDBDriver: true, EnableRoute: true, EnableFramework: true}}
+	r, err := http.NewRequest("GET", "hello/1", nil)
+	if err != nil {
+		t.Errorf("http.NewRequest('GET', 'hello/1', nil) returned unexpected error: %v", err)
+	}
 
+	ctx := core.ContextInject(r.Context(), httpnet.NewHTTPRequestExtractor(r, nil))
 	got := db.withComment(ctx, "Select 1")
 	want := "Select 1/*driver=database%2Fsql,framework=net%2Fhttp,route=hello%2F1*/"
-
 	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+		t.Errorf("db.withComment(ctx, 'Select 1') got %q, wanted %q", got, want)
 	}
 }
 
 func TestQueryWithSemicolon(t *testing.T) {
 	mockDB, _, err := sqlmock.New()
-
-	db := DB{DB: mockDB, options: CommenterOptions{EnableDBDriver: true}}
 	if err != nil {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
+
+	db := DB{DB: mockDB, options: core.CommenterOptions{EnableDBDriver: true}}
 	got := db.withComment(context.Background(), "Select 1;")
 	want := "Select 1/*driver=database%2Fsql*/;"
-
 	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+		t.Errorf("db.withComment(context.Background(), 'Select 1;') got %q, wanted %q", got, want)
 	}
 }
 
 func TestOtelIntegration(t *testing.T) {
 	mockDB, _, err := sqlmock.New()
-
-	db := DB{DB: mockDB, options: CommenterOptions{EnableTraceparent: true}}
 	if err != nil {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
 
+	db := DB{DB: mockDB, options: core.CommenterOptions{EnableTraceparent: true}}
 	exp, _ := stdouttrace.New(stdouttrace.WithPrettyPrint())
 	bsp := sdktrace.NewSimpleSpanProcessor(exp) // You should use batch span processor in prod
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithSpanProcessor(bsp),
 	)
-
 	ctx, _ := tp.Tracer("").Start(context.Background(), "parent-span-name")
 
 	got := db.withComment(ctx, "Select 1;")
-	r, _ := regexp.Compile("Select 1/\\*traceparent=\\d{1,2}-[a-zA-Z0-9_]{32}-[a-zA-Z0-9_]{16}-\\d{1,2}\\*/;")
+	wantRegex := "Select 1/\\*traceparent=\\d{1,2}-[a-zA-Z0-9_]{32}-[a-zA-Z0-9_]{16}-\\d{1,2}\\*/;"
+	r, err := regexp.Compile(wantRegex)
+	if err != nil {
+		t.Errorf("regex.Compile() failed with error: %v", err)
+	}
 
 	if !r.MatchString(got) {
-		t.Errorf("got %q", got)
+		t.Errorf("%q does not match the given regex %q", got, wantRegex)
 	}
 }
--- go/go-sql/go.mod
+++ go/go-sql/go.mod
@@ -2,19 +2,19 @@ module google.com/sqlcommenter/gosql
 
 go 1.19
 
-require (
-	go.opentelemetry.io/otel v1.10.0
-	go.opentelemetry.io/otel/sdk v1.10.0
-)
+require go.opentelemetry.io/otel/sdk v1.10.0
 
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/sqlcommenter/go/core v0.0.1-beta // indirect
+	go.opentelemetry.io/otel v1.10.0 // indirect
 	golang.org/x/sys v0.0.0-20220927170352-d9d178bc13c6 // indirect
 )
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/google/sqlcommenter/go/net/http v0.0.1-beta
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.10.0
 	go.opentelemetry.io/otel/trace v1.10.0 // indirect
 )
