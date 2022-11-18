1f4d5ffbe508e461add665bc6de1b75e1ad720db
Kapil Verma <kapv89@gmail.com>
2022-11-18 11:58:12 +0530

    database type in driver tag

--- go/database/sql/go-sql_test.go
+++ go/database/sql/go-sql_test.go
@@ -32,7 +32,7 @@ func TestDisabled(t *testing.T) {
 	if err != nil {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
-	db := DB{DB: mockDB, options: core.CommenterOptions{}}
+	db := DB{DB: mockDB, driverName: "mocksql", options: core.CommenterOptions{}}
 	query := "SELECT 2"
 	if got, want := db.withComment(context.Background(), query), query; got != want {
 		t.Errorf("db.withComment(context.Background(), %q) = %q, want = %q", query, got, want)
@@ -45,7 +45,7 @@ func TestHTTP_Net(t *testing.T) {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
 
-	db := DB{DB: mockDB, options: core.CommenterOptions{EnableDBDriver: true, EnableRoute: true, EnableFramework: true}}
+	db := DB{DB: mockDB, driverName: "mocksql", options: core.CommenterOptions{EnableDBDriver: true, EnableRoute: true, EnableFramework: true}}
 	r, err := http.NewRequest("GET", "hello/1", nil)
 	if err != nil {
 		t.Errorf("http.NewRequest('GET', 'hello/1', nil) returned unexpected error: %v", err)
@@ -53,7 +53,7 @@ func TestHTTP_Net(t *testing.T) {
 
 	ctx := core.ContextInject(r.Context(), httpnet.NewHTTPRequestExtractor(r, nil))
 	got := db.withComment(ctx, "Select 1")
-	want := "Select 1/*driver=database%2Fsql,framework=net%2Fhttp,route=hello%2F1*/"
+	want := "Select 1/*driver=database%2Fsql%3Amocksql,framework=net%2Fhttp,route=hello%2F1*/"
 	if got != want {
 		t.Errorf("db.withComment(ctx, 'Select 1') got %q, wanted %q", got, want)
 	}
@@ -65,9 +65,9 @@ func TestQueryWithSemicolon(t *testing.T) {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
 
-	db := DB{DB: mockDB, options: core.CommenterOptions{EnableDBDriver: true}}
+	db := DB{DB: mockDB, driverName: "mocksql", options: core.CommenterOptions{EnableDBDriver: true}}
 	got := db.withComment(context.Background(), "Select 1;")
-	want := "Select 1/*driver=database%2Fsql*/;"
+	want := "Select 1/*driver=database%2Fsql%3Amocksql*/;"
 	if got != want {
 		t.Errorf("db.withComment(context.Background(), 'Select 1;') got %q, wanted %q", got, want)
 	}
@@ -79,7 +79,7 @@ func TestOtelIntegration(t *testing.T) {
 		t.Fatalf("MockSQL failed with unexpected error: %s", err)
 	}
 
-	db := DB{DB: mockDB, options: core.CommenterOptions{EnableTraceparent: true}}
+	db := DB{DB: mockDB, driverName: "mocksql", options: core.CommenterOptions{EnableTraceparent: true}}
 	exp, _ := stdouttrace.New(stdouttrace.WithPrettyPrint())
 	bsp := sdktrace.NewSimpleSpanProcessor(exp) // You should use batch span processor in prod
 	tp := sdktrace.NewTracerProvider(
