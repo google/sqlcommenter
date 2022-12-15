3dc25a4a3c52992f737c54d284d35d346b41e2b4
kostyay <kostya.y@gmail.com>
2022-12-15 14:03:39 +0200

    escape

--- go/core/core.go
+++ go/core/core.go
@@ -80,9 +80,9 @@ func ConvertMapToComment(tags map[string]string) string {
 
 	for _, key := range sortedKeys {
 		if i == sz-1 {
-			sb.WriteString(fmt.Sprintf("%s=%s", encodeURL(key), encodeURL(tags[key])))
+			sb.WriteString(fmt.Sprintf("%s='%s'", encodeURL(key), encodeURL(tags[key])))
 		} else {
-			sb.WriteString(fmt.Sprintf("%s=%s,", encodeURL(key), encodeURL(tags[key])))
+			sb.WriteString(fmt.Sprintf("%s='%s',", encodeURL(key), encodeURL(tags[key])))
 		}
 		i++
 	}
