0b92255880544106ffa838c9174c98fac08e4ab3
gkatzioura <gkatzioura@gmail.com>
2023-01-15 23:00:17 +0000

    228: Added formatting

--- java/sqlcommenter-java/src/main/java/com/google/cloud/sqlcommenter/filter/SpringSQLCommenterWebFilter.java
+++ java/sqlcommenter-java/src/main/java/com/google/cloud/sqlcommenter/filter/SpringSQLCommenterWebFilter.java
@@ -1,36 +1,34 @@
 package com.google.cloud.sqlcommenter.filter;
 
+import com.google.cloud.sqlcommenter.threadlocalstorage.State;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
-
-import com.google.cloud.sqlcommenter.threadlocalstorage.State;
-
 import reactor.core.publisher.Mono;
 
 public class SpringSQLCommenterWebFilter implements WebFilter {
 
-    private final RequestMappingHandlerMapping handlerMapping;
+  private final RequestMappingHandlerMapping handlerMapping;
 
-    public SpringSQLCommenterWebFilter(RequestMappingHandlerMapping handlerMapping) {
-        this.handlerMapping = handlerMapping;
-    }
+  public SpringSQLCommenterWebFilter(RequestMappingHandlerMapping handlerMapping) {
+    this.handlerMapping = handlerMapping;
+  }
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        HandlerMethod handlerMethod = (HandlerMethod) this.handlerMapping.getHandler(exchange).toFuture().getNow(null);
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    HandlerMethod handlerMethod =
+        (HandlerMethod) this.handlerMapping.getHandler(exchange).toFuture().getNow(null);
 
-        State state = State.newBuilder()
-                           .withActionName(handlerMethod.getMethod().getName())
-                           .withFramework("spring")
-                           .withControllerName(handlerMethod.getBeanType().getSimpleName().replace("Controller",""))
-                           .build();
+    State state =
+        State.newBuilder()
+            .withActionName(handlerMethod.getMethod().getName())
+            .withFramework("spring")
+            .withControllerName(
+                handlerMethod.getBeanType().getSimpleName().replace("Controller", ""))
+            .build();
 
-        return chain
-                .filter(exchange)
-                .contextWrite(ctx -> ctx.put("state", state));
-    }
-
+    return chain.filter(exchange).contextWrite(ctx -> ctx.put("state", state));
+  }
 }
--- java/sqlcommenter-java/src/main/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionInvocationHandler.java
+++ java/sqlcommenter-java/src/main/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionInvocationHandler.java
@@ -1,38 +1,35 @@
 package com.google.cloud.sqlcommenter.r2dbc;
 
+import com.google.cloud.sqlcommenter.threadlocalstorage.State;
+import io.r2dbc.spi.Connection;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-
-import com.google.cloud.sqlcommenter.threadlocalstorage.State;
-
-import io.r2dbc.spi.Connection;
 import reactor.util.context.ContextView;
 
 public class ConnectionInvocationHandler implements InvocationHandler {
 
-    private final Connection connection;
-    private final ContextView contextView;
+  private final Connection connection;
+  private final ContextView contextView;
 
-    public ConnectionInvocationHandler(Connection connection, ContextView contextView) {
-        this.connection = connection;
-        this.contextView = contextView;
+  public ConnectionInvocationHandler(Connection connection, ContextView contextView) {
+    this.connection = connection;
+    this.contextView = contextView;
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    String methodName = method.getName();
+
+    if ("createStatement".equals(methodName)) {
+      String query = (String) args[0];
+      if (contextView != null) {
+        State state = contextView.get("state");
+        query = state.formatAndAppendToSQL(query);
+      }
+
+      return method.invoke(connection, query);
+    } else {
+      return method.invoke(connection, args);
     }
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String methodName = method.getName();
-
-        if ("createStatement".equals(methodName)) {
-            String query = (String) args[0];
-            if(contextView!=null) {
-                State state = contextView.get("state");
-                query = state.formatAndAppendToSQL(query);
-            }
-
-            return method.invoke(connection, query);
-        } else {
-            return method.invoke(connection, args);
-        }
-    }
-
+  }
 }
