7b8da8aea3691096a04d83a968020e7d88bfd61f
gkatzioura <gkatzioura@gmail.com>
2023-01-15 22:41:59 +0000

    228: Added unchecked exception

--- java/sqlcommenter-java/src/main/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionFactoryAspect.java
+++ java/sqlcommenter-java/src/main/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionFactoryAspect.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.reactivestreams.Publisher;
 
-import io.r2dbc.spi.Connection;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Operators;
 
@@ -16,11 +15,11 @@ public class ConnectionFactoryAspect {
 
     @Around("execution(* io.r2dbc.spi.ConnectionFactory.create(..)) ")
     public Object beforeSampleCreation(ProceedingJoinPoint joinPoint) throws Throwable {
-        Publisher<? extends Connection> publisher = (Publisher<? extends Connection>) joinPoint.proceed();
+        Object object = joinPoint.proceed();
 
+        @SuppressWarnings("unchecked") Publisher<Object> publisher = (Publisher<Object>) object;
 
         return Mono.from(publisher)
-                   .cast(Object.class)
                    .transform(Operators.liftPublisher((publisher1, coreSubscriber) -> new ConnectionDecorator(coreSubscriber)));
     }
 
