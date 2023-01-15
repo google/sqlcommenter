8628aaa2bd4df98cd7c64b9582d95f421a908ff5
gkatzioura <gkatzioura@gmail.com>
2023-01-15 23:01:13 +0000

    228: Added dependencies and test

--- java/sqlcommenter-java/src/test/java/com/google/cloud/sqlcommenter/filter/SpringSQLCommenterWebFilterTest.java
+++ java/sqlcommenter-java/src/test/java/com/google/cloud/sqlcommenter/filter/SpringSQLCommenterWebFilterTest.java
@@ -8,33 +8,32 @@ import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.web.reactive.result.method.RequestMappingInfo;
 import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.server.WebFilterChain;
-
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
 
 @RunWith(JUnit4.class)
 public class SpringSQLCommenterWebFilterTest {
 
-    @Test
-    public void testPreHandlePlacesStateInContextView() throws NoSuchMethodException {
-        RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
-        RequestMappingInfo info = RequestMappingInfo.paths("/test").build();
-        requestMappingHandlerMapping.registerMapping(info, this, SpringSQLCommenterWebFilterTest.class.getMethod("testPreHandlePlacesStateInContextView"));
+  @Test
+  public void testPreHandlePlacesStateInContextView() throws NoSuchMethodException {
+    RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+    RequestMappingInfo info = RequestMappingInfo.paths("/test").build();
+    requestMappingHandlerMapping.registerMapping(
+        info,
+        this,
+        SpringSQLCommenterWebFilterTest.class.getMethod("testPreHandlePlacesStateInContextView"));
 
-        SpringSQLCommenterWebFilter springSQLCommenterWebFilter = new SpringSQLCommenterWebFilter(requestMappingHandlerMapping);
+    SpringSQLCommenterWebFilter springSQLCommenterWebFilter =
+        new SpringSQLCommenterWebFilter(requestMappingHandlerMapping);
 
-        WebFilterChain filterChain = filterExchange -> Mono.empty();
+    WebFilterChain filterChain = filterExchange -> Mono.empty();
 
-        MockServerWebExchange exchange = MockServerWebExchange.from(
-                MockServerHttpRequest
-                        .get("/test"));
+    MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/test"));
 
-        StepVerifier.create(springSQLCommenterWebFilter.filter(exchange, filterChain))
-                .expectAccessibleContext()
-                .hasKey("state")
-                .then()
-                .verifyComplete();
-    }
-
+    StepVerifier.create(springSQLCommenterWebFilter.filter(exchange, filterChain))
+        .expectAccessibleContext()
+        .hasKey("state")
+        .then()
+        .verifyComplete();
+  }
 }
