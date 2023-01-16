682b8c144caaa0d979bbb7a322313233d4dbcda7
gkatzioura <gkatzioura@gmail.com>
2023-01-16 08:10:07 +0000

    228: Added unit test

---
+++ java/sqlcommenter-java/src/test/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionDecoratorTest.java
@@ -0,0 +1,63 @@
+package com.google.cloud.sqlcommenter.r2dbc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.r2dbc.spi.Connection;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.reactivestreams.Subscription;
+import reactor.core.CoreSubscriber;
+
+@RunWith(JUnit4.class)
+public class ConnectionDecoratorTest extends TestCase {
+
+  private CoreSubscriber<Object> delegate;
+  private ConnectionDecorator connectionDecorator;
+
+  @Override
+  @Before
+  public void setUp() throws Exception {
+    delegate = mock(CoreSubscriber.class);
+    connectionDecorator = new ConnectionDecorator(delegate);
+  }
+
+  @Test
+  public void testOnSubscribe() {
+    Subscription mocked = mock(Subscription.class);
+    connectionDecorator.onSubscribe(mocked);
+    verify(delegate, times(1)).onSubscribe(mocked);
+  }
+
+  @Test
+  public void testOnNext() {
+    Connection connection = mock(Connection.class);
+    connectionDecorator.onNext(connection);
+    verify(delegate, times(1)).currentContext();
+    verify(delegate, times(1)).onNext(any());
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testOnNextNonConnection() {
+    Object object = new Object();
+    connectionDecorator.onNext(object);
+  }
+
+  @Test
+  public void testOnError() {
+    Throwable mocked = mock(Throwable.class);
+    connectionDecorator.onError(mocked);
+    verify(delegate, times(1)).onError(mocked);
+  }
+
+  @Test
+  public void testOnComplete() {
+    connectionDecorator.onComplete();
+    verify(delegate, times(1)).onComplete();
+  }
+}
