9ede1fe9963bd0e3829d9067c7ba8c34e83dc38d
gkatzioura <gkatzioura@gmail.com>
2023-01-16 10:20:51 +0000

    228: Formatted

--- java/sqlcommenter-java/src/test/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionFactoryAspectTest.java
+++ java/sqlcommenter-java/src/test/java/com/google/cloud/sqlcommenter/r2dbc/ConnectionFactoryAspectTest.java
@@ -1,5 +1,11 @@
 package com.google.cloud.sqlcommenter.r2dbc;
 
+import static org.junit.Assert.assertEquals;
+
+import com.google.cloud.sqlcommenter.threadlocalstorage.State;
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Statement;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactivestreams.Publisher;
@@ -8,13 +14,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import com.google.cloud.sqlcommenter.threadlocalstorage.State;
-
-import io.r2dbc.spi.Connection;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.Statement;
-import static org.junit.Assert.assertEquals;
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
 
@@ -23,30 +22,29 @@ import reactor.util.context.Context;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ConnectionFactoryAspectTest {
 
-    private final String stmt1 = "SELECT * from FOO";
+  private final String stmt1 = "SELECT * from FOO";
 
-    @Autowired
-    private ConnectionFactory connectionFactory;
+  @Autowired private ConnectionFactory connectionFactory;
 
-    @Test
-    public void name() {
-        State state =
-                State.newBuilder()
-                     .withControllerName("Order")
-                     .withFramework("spring")
-                     .withActionName("add")
-                     .build();
-        Context updatedContext = Context.of("state", state);
+  @Test
+  public void name() {
+    State state =
+        State.newBuilder()
+            .withControllerName("Order")
+            .withFramework("spring")
+            .withActionName("add")
+            .build();
+    Context updatedContext = Context.of("state", state);
 
-        Publisher<? extends Connection> conn = connectionFactory.create();
+    Publisher<? extends Connection> conn = connectionFactory.create();
 
-        Statement statement = Mono.from(conn)
-                             .contextWrite(updatedContext)
-                             .map(connection -> connection.createStatement(stmt1))
-                             .block();
+    Statement statement =
+        Mono.from(conn)
+            .contextWrite(updatedContext)
+            .map(connection -> connection.createStatement(stmt1))
+            .block();
 
-        String value = (String) ReflectionTestUtils.getField(statement, null, "sql");
-        assertEquals(value, state.formatAndAppendToSQL(stmt1));
-    }
-
+    String value = (String) ReflectionTestUtils.getField(statement, null, "sql");
+    assertEquals(value, state.formatAndAppendToSQL(stmt1));
+  }
 }
